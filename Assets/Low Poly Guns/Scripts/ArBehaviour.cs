00961f57c2a82307374d55d77b0724eeeb1022a9
OJASBRID <ojasbc21@iitk.ac.in>
2022-06-23 23:21:27 +0530

    Enemies

--- Assets/Low Poly Guns/Scripts/ArBehaviour.cs
+++ Assets/Low Poly Guns/Scripts/ArBehaviour.cs
@@ -14,17 +14,18 @@ public class ArBehaviour : MonoBehaviour
     public int currentAmmo;
     public float reloadTime = 1f;
     private bool isReloading = false;
-    private float Delay= 0f;
+    private float Delay = 0f;
     public Animator animator;
-         
+    public int totalammo;
+
     public Recoil recoilScript;
     //public GameObject FleshImpactEffect;
     // public GameObject ElseImpactEffect;
 
     private void Start()
-    { 
-        
-      currentAmmo = maxAmmo;
+    {
+
+        currentAmmo = maxAmmo;
 
     }
 
@@ -37,18 +38,18 @@ public class ArBehaviour : MonoBehaviour
     {
         if (isReloading)
             return;
-        if (currentAmmo <= 0 || Input.GetKeyDown(KeyCode.R)) 
+        if ((currentAmmo <= 0 || Input.GetKeyDown(KeyCode.R)) && totalammo != 0)
         {
             StartCoroutine(Reload());
             return;
         }
         if (firetype)
-        { 
-        if (Input.GetButton("Fire1") && Time.time >= Delay)
         {
-            Delay = Time.time + 1f / firerate;
-            Shoot(firetype);
-        }
+            if (Input.GetButton("Fire1") && Time.time >= Delay)
+            {
+                Delay = Time.time + 1f / firerate;
+                Shoot(firetype);
+            }
         }
 
         else
@@ -65,19 +66,34 @@ public class ArBehaviour : MonoBehaviour
         isReloading = true;
         Debug.Log("Reloading...");
         animator.SetBool("Reloading", true);
-        yield return new WaitForSeconds(reloadTime-.25f);
+        yield return new WaitForSeconds(reloadTime - .25f);
         animator.SetBool("Reloading", false);
         yield return new WaitForSeconds(.25f);
+
+        if (totalammo < maxAmmo && totalammo > 0)
+        {
+            currentAmmo = totalammo;
+            totalammo = 0;
+
+        }
+        else
+        { 
+        totalammo = totalammo - maxAmmo + currentAmmo;
         currentAmmo = maxAmmo;
+        }
+
+        
+        
         isReloading = false;
 
     }
     void Shoot(bool value)
-    {
+    { if (!(currentAmmo + totalammo <= 0))
+        { 
         currentAmmo--;
         if (value)
-        { 
-        recoilScript.RecoilFire1();
+        {
+            recoilScript.RecoilFire1();
         }
 
         else
@@ -86,22 +102,22 @@ public class ArBehaviour : MonoBehaviour
         }
         flash.Play();
         RaycastHit hit;
-        if(Physics.Raycast(cam.transform.position,cam.transform.forward,out hit,range))
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
         {
             EnemyDamage target = hit.transform.GetComponent<EnemyDamage>();
-            
+
             {
-                 if(target != null)
+                if (target != null)
                 {
                     target.Damage(damage);
-                   // GameObject impact1 = Instantiate(FleshImpactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                    // GameObject impact1 = Instantiate(FleshImpactEffect, hit.point, Quaternion.LookRotation(hit.normal));
                     //Destroy(impact1, 1.5f);
                 }
-                
+
                 //GameObject impact2 = Instantiate(ElseImpactEffect, hit.point, Quaternion.LookRotation(hit.normal));
                 //Destroy(impact2, 1.5f);
-            } 
+            }
         }
     }
-    
+}
 }
--- Assets/Scripts/EnemyNavMesh.cs
+++ Assets/Scripts/EnemyNavMesh.cs
@@ -7,6 +7,8 @@ public class EnemyNavMesh : MonoBehaviour
 {
     [SerializeField] private Transform movePositionTransform;
     private NavMeshAgent navMeshAgent;
+    public Animator animator;
+    public float range;
    private void Awake()
    {
        navMeshAgent = GetComponent<NavMeshAgent>();
@@ -15,5 +17,13 @@ public class EnemyNavMesh : MonoBehaviour
    private void Update()
    {
        navMeshAgent.destination = movePositionTransform.position;
-   }
+        if (Vector3.Distance(movePositionTransform.position, gameObject.transform.position) < range)
+        {
+            animator.SetBool("InRange", true);
+        }
+        else
+        {
+            animator.SetBool("InRange", false);
+        }
+    }
 }
