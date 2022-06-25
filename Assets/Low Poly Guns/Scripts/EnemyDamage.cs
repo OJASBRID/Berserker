7aa2c3bb5675d9fed93c1e92ca2ac05c973c1d63
OJASBRID <ojasbc21@iitk.ac.in>
2022-06-25 15:18:49 +0530

    Changes to Zombies

--- Assets/Low Poly Guns/Scripts/EnemyDamage.cs
+++ Assets/Low Poly Guns/Scripts/EnemyDamage.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 public class EnemyDamage : MonoBehaviour
 {
     public float health = 100f;
+    public GameObject gameObjects;
     public void Damage(float amount)
     {
         health -= amount;
         if(health <= 0)
         {
             Destroy(gameObject);
-            
+            Instantiate(gameObjects, gameObject.transform);
         }
     }
 
