08450d1dddf1834adeba7cf1fba528446e1e09a3
OJASBRID <ojasbc21@iitk.ac.in>
2022-07-10 13:41:01 +0530

    Changes to player

---
+++ Assets/Scripts/ObjDest.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjDest : MonoBehaviour
+{
+    public GameObject Player;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == Player.transform)
+        {
+
+            Destroy(gameObject);
+        }
+
+    }
+}
